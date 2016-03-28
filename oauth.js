befd9c101abd17b798243f9e22d61722946cc5d1
Miguel Santamaria <msantamaria@hugeinc.com>
2016-03-28 11:56:10 -0500

    Removing config from gitignore

---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+    google: {
+        clientID: '367115757681-g2u3lq43tde98ku238jjr3jeng411pl6.apps.googleusercontent.com',
+        clientSecret: 'M3L1ASAlyEpaXHWRB1CUCkXQ',
+        callbackURL: 'https://contact-list-application.herokuapp.com/auth/google/callback'
+    }
+}
+
+module.exports = ids;
